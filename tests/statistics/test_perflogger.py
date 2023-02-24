2e8d917e6f8ce66b8c41d033bf2cfd724dc57445
Bryce Ferenczi <bryce.ferenczi@monash.edu>
2023-02-24 20:10:18 +1100

    few more tests for perflogger
    Signed-off-by: Bryce Ferenczi <bryce.ferenczi@monash.edu>

--- tests/statistics/test_perflogger.py
+++ tests/statistics/test_perflogger.py
@@ -8,22 +8,42 @@ from konductor.metadata import PerfLogger, PerfLoggerConfig
 @pytest.fixture
 def scalar_perf(tmp_path):
     config = PerfLoggerConfig(
-        write_path=tmp_path / "perf.pq",
+        write_path=tmp_path / "perf.parquet",
         train_buffer_length=100,
         validation_buffer_length=10,
-        statistics={"loss": ScalarStatistic},
+        statistics={"loss": ScalarStatistic, "accuracy": ScalarStatistic},
     )
     return PerfLogger(config)
 
 
-def test_writing(scalar_perf: PerfLogger):
+def test_forgot_train_or_val(scalar_perf: PerfLogger):
     with pytest.raises(AssertionError):
         scalar_perf.log("loss", {"blah": 0})
 
+
+def test_forgot_iteration(scalar_perf: PerfLogger):
+    scalar_perf.train()
+    with pytest.raises(AssertionError):
+        scalar_perf.log(
+            "loss", {"l2": randint(0, 10) / 10, "mse": randint(0, 100) / 10}
+        )
+
+
+def test_bad_statistic_key(scalar_perf: PerfLogger):
+    scalar_perf.train()
+    scalar_perf.set_iteration(0)
+    with pytest.raises(KeyError):
+        scalar_perf.log("nonexist", {"foo": 123})
+
+
+def test_writing_no_issue(scalar_perf: PerfLogger):
     scalar_perf.train()
     for i in range(100):
         scalar_perf.set_iteration(i)
         scalar_perf.log(
             "loss", {"l2": randint(0, 10) / 10, "mse": randint(0, 100) / 10}
+        )
+        scalar_perf.log(
+            "accuracy", {"l2": randint(0, 10) / 10, "mse": randint(0, 100) / 10}
         )
     scalar_perf.flush()
