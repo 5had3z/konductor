cd94704d03bb444ab23f29ad31d8e548dabdd0fa
Bryce Ferenczi <bryce.ferenczi@monash.edu>
2023-07-12 09:05:44 +1000

    add params for pytorch optimizers
    Signed-off-by: Bryce Ferenczi <bryce.ferenczi@monash.edu>

--- src/konductor/modules/optimizers/_pytorch/common.py
+++ src/konductor/modules/optimizers/_pytorch/common.py
@@ -1,4 +1,5 @@
-from dataclasses import dataclass
+from typing import Tuple
+from dataclasses import dataclass, field
 
 from torch import nn
 from torch.optim import Adam, AdamW, SGD
@@ -10,6 +11,10 @@ from ...optimizers import REGISTRY
 @dataclass
 @REGISTRY.register_module("Adam")
 class AdamConfig(PytorchOptimizer):
+    betas: Tuple[float, float] = field(default_factory=lambda: (0.9, 0.999))
+    eps: float = 1e-8
+    weight_decay: float = 0.0
+
     def get_instance(self, model: nn.Module):
         return self._apply_extra(Adam, model)
 
@@ -17,6 +22,10 @@ class AdamConfig(PytorchOptimizer):
 @dataclass
 @REGISTRY.register_module("SGD")
 class SGDConfig(PytorchOptimizer):
+    momentum: float = 0.0
+    dampening: float = 0.0
+    weight_decay: float = 0.0
+
     def get_instance(self, model: nn.Module):
         return self._apply_extra(SGD, model)
 
@@ -24,5 +33,9 @@ class SGDConfig(PytorchOptimizer):
 @dataclass
 @REGISTRY.register_module("AdamW")
 class AdamWConfig(PytorchOptimizer):
+    betas: Tuple[float, float] = field(default_factory=lambda: (0.9, 0.999))
+    eps: float = 1e-8
+    weight_decay: float = 1e-2
+
     def get_instance(self, model: nn.Module):
         return self._apply_extra(AdamW, model)
