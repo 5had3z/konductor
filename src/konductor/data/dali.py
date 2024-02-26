be8c83a71b36354eac64e10c5ab437b2bf879aad
Bryce Ferenczi <bryce.ferenczi@monash.edu>
2024-02-26 11:59:46 +1100

    add kwargs for dali pipeline + minor tweaks

--- src/konductor/data/dali.py
+++ src/konductor/data/dali.py
@@ -1,9 +1,8 @@
 from dataclasses import dataclass
-from typing import List
 
 import torch
 from nvidia.dali.plugin.pytorch import DALIGenericIterator
-from nvidia.dali.plugin.pytorch import LastBatchPolicy
+from nvidia.dali.plugin.base_iterator import LastBatchPolicy
 
 from . import DataloaderConfig, DATALOADER_REGISTRY, Registry
 from ..utilities.comm import get_rank, get_world_size
@@ -14,22 +13,27 @@ DALI_AUGMENTATIONS = Registry("DALI_AUGMENTATIONS")
 @dataclass
 @DATALOADER_REGISTRY.register_module("DALI")
 class DaliLoaderConfig(DataloaderConfig):
+    py_num_workers: int = 1
+    prefetch_queue_depth: int = 2
+
     def pipe_kwargs(self):
         """Common keyword arguments used for pipeline definition"""
         return {
             "shard_id": get_rank(),
             "num_shards": get_world_size(),
-            "num_threads": max(self.workers, 1),
+            "num_threads": max(self.workers, 1),  # Should have at least one thread
             "device_id": torch.cuda.current_device(),
             "batch_size": self.batch_size // get_world_size(),
             "augmentations": self.augmentations,
             "random_shuffle": self.shuffle,
+            "py_num_workers": self.py_num_workers,
+            "prefetch_queue_depth": self.prefetch_queue_depth,
         }
 
     def get_instance(
         self,
         pipelines,
-        out_map: List[str],
+        out_map: list[str],
         size: int = -1,
         reader_name: str | None = None,
     ):
